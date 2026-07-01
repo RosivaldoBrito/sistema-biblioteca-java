4989ae62fe47786ddf1dd2b8cf91a4d09a569732
Rosivaldo-Brito <rosivaldo.tb.r@gmail.com>
2026-07-01 18:11:12 -0300

    feat: Implementa edição de livros e novos atributos de acervo
    Adição dos atributos de ano de publicação e quantidade em estoque na entidade Livro. Criação do método auxiliar de busca por título (ignorando case sensitive) e implementação da funcionalidade de Modificar Livro (Update) no sub-menu.

--- src/entities/Livro.java
+++ src/entities/Livro.java
@@ -1,44 +1,52 @@
 package entities;
 
+
 import entities.enums.Categoria;
 
 public class Livro {
 	private String titulo;
 	private String autor;
 	private Categoria categoria;
+	private Integer anoPublicacao;
+	private Integer quantidadeEmEstoque;
 	
-	public Livro(String titulo, String autor, Categoria categoria) {
+	public Livro(String titulo, String autor, Categoria categoria, Integer anoPublicacao, Integer quantidadeEmEstoque) {
 		this.titulo = titulo;
 		this.autor = autor;
 		this.categoria = categoria;
+		this.anoPublicacao = anoPublicacao;
+		this.quantidadeEmEstoque = quantidadeEmEstoque;
 	}
-
 	public String getTitulo() {
 		return titulo;
 	}
-
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-
 	public String getAutor() {
 		return autor;
 	}
-
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
-
 	public Categoria getCategoria() {
 		return categoria;
 	}
-
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
 	}
+	public Integer getAnoPublicacao() {
+		return anoPublicacao;
+	}
+	public void setAnoPublicacao(Integer anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+	public Integer getQuantidadeEmEstoque() {
+		return quantidadeEmEstoque;
+	}
+	public void setQuantidadeEmEstoque(Integer quantidadeEmEstoque) {
+		this.quantidadeEmEstoque = quantidadeEmEstoque;
+	}
 	
 	
-	
-	
-
 }
